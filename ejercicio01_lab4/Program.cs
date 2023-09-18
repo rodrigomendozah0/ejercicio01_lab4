8180813f9a8ec55c4ac5fb4c75a34309528c8523
Rodrigo <Rodrigo@HP_RODRIGO>
2023-09-17 22:40:14 -0500

    Agregar archivos de proyecto.

---
+++ ejercicio01_lab4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio01_lab4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1 = 16;
+            int numero2 = 5;
+            int resultado = 0;
+
+            for (int i = 0; i < numero2; i++)
+            {
+                resultado = resultado + numero1;
+            }
+
+            Console.WriteLine("El resultado de esta multplicación es: " + resultado);
+            Console.ReadKey();
+        }
+    }
+}
